227086fcff1680ff49cb00cf54a929c5fc991c88
Sihkdragon <dithor37@gmail.com>
2022-03-10 07:51:53 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        myblue:{
+          1:'#3C40C6',
+          2:'#575FCF'
+        },
+        myred:'#FF5678',
+        mygray:{
+          1:'#1E272E',
+          2:'#485460'
+        }
+      }
+    },
+  },
+  plugins: [],
+}
